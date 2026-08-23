0003414ad93208d57c472e4654adb0f00f547918
Nick <namct147@gmail.com>
2026-08-23 10:31:05 +0700

    feat: implement checksum calculate and verify functions

---
+++ internet/checksum.go
@@ -0,0 +1,42 @@
+package internet
+
+import (
+	"encoding/binary"
+)
+
+func CalculateChecksum(data []byte) uint16 {
+	var sum uint32
+
+	for i := 0; i < len(data)-1; i += 2 {
+		word := binary.BigEndian.Uint16(data[i : i+2])
+		sum += uint32(word)
+	}
+
+	if len(data)%2 != 0 {
+		sum += uint32(data[len(data)-1]) << 8
+	}
+
+	for sum > 0xFFFF {
+		carry := sum >> 16
+		sum = (sum & 0xFFFF) + carry
+	}
+
+	return uint16(^sum)
+}
+
+func VerifyChecksum(headerBytes []byte) bool {
+	if len(headerBytes) < 12 {
+		return false
+	}
+	ogChecksum := binary.BigEndian.Uint16(headerBytes[10:12])
+
+	headerCopy := make([]byte, len(headerBytes))
+	copy(headerCopy, headerBytes)
+
+	headerCopy[10] = 0
+	headerCopy[11] = 0
+
+	calculate := CalculateChecksum(headerCopy)
+
+	return calculate == ogChecksum
+}
